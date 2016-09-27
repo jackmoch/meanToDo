8e7b68718e283b0836f95a0adb8b2fcfbf63d27f
Jack Mocherman <mochermanjack@gmail.com>
2016-09-27 11:48:37 -0500

    update function working

--- client/app/main.js
+++ client/app/main.js
@@ -12,6 +12,10 @@ angular
 				controller: 'ToDoCtrl',
 				templateUrl: 'partials/list.html'
 			})
+			.when('/item/:id', {
+				controller: 'ItemCtrl',
+				templateUrl: 'partials/item.html'
+			})
 	})
 	.controller('MainCtrl', function ($scope, $http) {
 
@@ -19,7 +23,7 @@ angular
 			.then(({data : {title}}) => $scope.title = title)
 
 	})
-	.controller('ToDoCtrl', function($scope, $http) {
+	.controller('ToDoCtrl', function($scope, $http, $location) {
 
 		let pageLoad = function() {
 			$http.get('/api/items')
@@ -43,6 +47,12 @@ angular
 				.catch(console.error)
 		}
 
+		$scope.editItem = function() {
+			const id = this.item._id
+
+			$location.path(`/item/${id}`)
+		}
+
 		$scope.deleteItem = function() {
 			const id = this.item._id
 
@@ -51,5 +61,31 @@ angular
 				.then(() => pageLoad())
 				.catch(console.error)
 		}
+
+	})
+	.controller('ItemCtrl', function($scope, $http, $location, $routeParams) {
+
+	  $scope.currentItemId = $routeParams.id
+
+		$http
+			.get(`/api/item/${$scope.currentItemId}`)
+			.then((data) => {
+				const item = data.data.item[0]
+				$scope.content = item.content
+				$scope.date = item.date
+			})
+
+			$scope.updateItem = function() {
+
+				const item = {
+					content: $scope.content,
+					date: $scope.date
+				}
+
+				$http
+					.put(`/api/items/${$scope.currentItemId}`, item)
+					.then(() => $location.path('/list'))
+					.catch(console.error)
+			}
 
 	})
--- server/server.js
+++ server/server.js
@@ -25,9 +25,7 @@ const Item = mongoose.model('item', {
 app.get('/api/items', (req, res, err) => 
 	Item
 		.find({})
-		.then(messages => 
-		res.json({ messages })
-		)
+		.then(messages => res.status(200).json({ messages }))
 		.catch(err)
 )
 
@@ -35,7 +33,16 @@ app.post('/api/items', (req, res, err) => {
 	const item = req.body
 	Item
 		.create(item)
-		.then(item => res.json(item))
+		.then(item => res.status(201).json(item))
+		.catch(err)
+})
+
+app.put('/api/items/:id', (req, res, err) => {
+	const id = req.params.id
+	const item = req.body
+	Item
+		.findOneAndUpdate({_id: id}, item, { upsert: true })
+		.then(data => res.status(200).json(data))
 		.catch(err)
 })
 
@@ -43,9 +50,24 @@ app.delete('/api/items/:id', (req, res, err) => {
 	const id = req.params.id
 	Item
 		.remove({_id: id})
-		.then(data => res.json(data))
+		.then(data => res.status(204).json(data))
 		.catch(err)
 })
+
+app.get('/api/item/:id', (req, res, err) => {
+	const id = req.params.id
+	Item
+		.find({_id: `${id}`})
+		.then(item => res.status(200).json({ item }))
+})
+
+app.use((req, res) => 
+	res.status(404).send({ code: 404, status: 'Not Found' })
+)
+
+app.use((err, req, res, next) => 
+	res.status(500).send({ code: 500, status: 'Internal Server Error' })
+)
 
 mongoose.Promise = Promise
 mongoose.connect(MONGODB_URL, () => {
